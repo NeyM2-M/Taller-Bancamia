735cac124d74b4efc3753b7923907f10d842dc6b
Neym7 <disney.matiz@digitalprof-it.com>
2026-04-24 13:36:59 -0500

    Create numerosAleatorios.py

---
+++ numerosAleatorios.py
@@ -0,0 +1,27 @@
+import random
+
+aprobadas = 0
+rechazadas = 0
+sospechoso = 0
+
+cantidadNumeros = int(input("Ingrese un número:  "))
+for i in range (cantidadNumeros):
+    numerosAleatorios = random.randint(100000, 999999)
+
+    if numerosAleatorios %10 == 5:
+        print(f" {numerosAleatorios} - Sospechoso")
+        sospechoso += 1
+    elif numerosAleatorios %2 == 0:
+        print(f" {numerosAleatorios} - Aprobado")
+        aprobadas += 1
+    else:
+        print(f" {numerosAleatorios} - Rechazado")
+        rechazadas += 1
+
+
+print("\n")
+#Resumen numeros aleatorios
+print("Resumen de Números Aleatorios")
+print(f"Total Aprobadas:  {aprobadas}")
+print(f"Total Rechazadas: {rechazadas}")
+print(f"Total Sospechosos:  {sospechoso}")
